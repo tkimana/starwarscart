37346eecd0d60a309fc061a0df38373217bdf41e
Theodore Kimana <kimana.theodore@gmail.com>
2019-06-05 16:24:35 -0500

    d

--- src/DropDown.js
+++ src/DropDown.js
@@ -5,14 +5,16 @@ import DropDownItem from './dropDownItem';
 export default class DropDown extends React.Component {
   render() {
     return (
-      <div style={{"background":"white","position":"absolute","zIndex":"1","borderStyle":"solid","borderWidth":"3px","width":"300px","right":"0px"}}>
-        {this.props.items.map(item => {return (item.quantity ? <DropDownItem item={item} deleteFromCart={this.props.deleteFromCart} plusOne={this.props.plusOne} minusOne={this.props.minusOne}/> : null)})}
+      <div style={{"background":"secondary","position":"absolute","zIndex":"1","borderStyle":"solid","borderWidth":"3px","width":"300px","right":"0px"}}>
+
+  {this.props.items.map(item => {return (item.quantity ? <DropDownItem item={item} deleteFromCart={this.props.deleteFromCart} plusOne={this.props.plusOne} minusOne={this.props.minusOne}/> : null)})}
         {
-          (this.props.items.find( item => {return item.quantity !== undefined} ) !== undefined)
+
+  (this.props.items.find( item => {return item.quantity !== undefined} ) !== undefined)
           ? ``
           : `Nothing in the cart!`
         }
-      </div>
+      </div> 
     );
   }
 }
